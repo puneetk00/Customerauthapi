2ca5e1892b5a257242a74d2ff5881c20f004fee7
Customer <u2-yxkwugyqjzn9@c98252.sgvps.net>
2021-03-01 10:42:33 +0000

    files

--- Api/CustomerRepositoryInterface.php
+++ Api/CustomerRepositoryInterface.php
@@ -5,16 +5,10 @@ namespace Jaybro\Customerauthapi\Api;
 
 interface CustomerRepositoryInterface
 {
-   
-
     /**
      * [getCustomerInfo description]
-     * @param  string $username [description]
-     * @param  string $password [description]
      *  @return mixed
      */
-	public function getCustomerInfo($email);
-
-	
+	public function getCustomerInfo();
 
 }
--- Model/CustomerRepository.php
+++ Model/CustomerRepository.php
@@ -11,53 +11,76 @@ class CustomerRepository implements CustomerRepositoryInterface
     private $_cusomerFactory;
     private $_context;
     private $_customerRepository;
-
-    public function __construct(CustomerFactory $CustomerFactory,Context $Context, CutomerRepInterface $CustomerRepositoryInterface)
+    protected $userContext;
+    
+	
+	public function __construct(
+	CustomerFactory $CustomerFactory,Context $Context, 
+	CutomerRepInterface $CustomerRepositoryInterface,
+	\Magento\Authorization\Model\CompositeUserContext $userContext)
     {
+		 $this->userContext = $userContext;
         $this->_customerFactory = $CustomerFactory;
         $this->_context = $Context;
         $this->_customerRepository = $CustomerRepositoryInterface;
     	
 
     }
-
-    public function getCustomerInfo($email)
+	
+	/**
+     * 
+     * @return int
+     */
+    public function getCustomerId()
     {
-      /*  $userData = array("username" => $username, "password" => $password);
-        $ch = curl_init("http://40.121.65.234:92/rest/V1/integration/customer/token");
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($userData));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Content-Lenght: " . strlen(json_encode($userData))));
-    
-        $token = curl_exec($ch); 
-        
-    
-       $ch = curl_init("http://40.121.65.234:92/rest/V2/jaybro_customerauthapi/". $username . "/". $password);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($userData));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Authorization: Bearer " . json_decode($token)));
-    
-        $result = curl_exec($ch); 
-        var_dump($result->geId);
+        return $customerId = $this->userContext->getUserId();
+    }
 
-        
-
-         */
-           $websiteId = 1;
-           $customerinfo = $this->_customerRepository->get($email,$websiteId);
-       
-
-           $customerId = $customerinfo->getId();
-
-
-
-          $customer = $this->_customerFactory->create()->load($customerId);
+    public function getCustomerInfo()
+    {
+		
+           $customerId = $this->getCustomerId();
+		   $customer = $this->_customerFactory->create()->load($customerId);
+           $customerinfo = $this->_customerRepository->get($customer->getEmail());
+           $cusomeruid =  $customerinfo->getCustomAttribute('p21customerid')->getValue();
          
 
-          return json_encode($customer->getData()) ;
+       //   return json_encode($customer_Data) ;
          
+
+        $customerAddressData = [];
+        $customerAddress = [];
+ 
+        if ($customer->getAddresses() != null)
+        {
+            foreach ($customer->getAddresses() as $address) {
+                $customerAddress[] = $address->toArray();
+            }
+        }
+ 
+        if ($customerAddress != null)
+        {
+            foreach ($customerAddress as $customerAddres)
+            {
+                $companyname = $customerAddres['company'];
+                $street = $customerAddres['street'];
+                $city = $customerAddres['city'];
+                $region = $customerAddres['region'];
+                $country = $customerAddres['country_id'];
+                $postcode = $customerAddres['postcode'];
+                $telephone = $customerAddres['telephone'];
+               // $cutomer_pid = $customerAddres['p21contactid'];
+ 
+               // $customerAddressData[] = $customerAddres->toArray();
+               $customerAddressData = array("Street" => $street, "City" => $city,"Region" => $region,"Country" => $country,"Postcode" => $postcode,);
+
+
+            }
+        }
+    // return $customerAddressData;
+
+    $customer_Data = array("First Name" => $customer->getData('firstname'), "Last Name" =>  $customer->getData('lastname'), "Email Address" => $customer->getEmail() , "Phone Number" => $telephone, "Delivery address" => $customerAddressData,"Company Name" => $companyname,"Customer Unique ID" => $cusomeruid);
+      return json_encode($customer_Data) ;
 
           
 
